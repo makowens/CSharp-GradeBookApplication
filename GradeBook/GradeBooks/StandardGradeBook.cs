486b6de1619e0d6f86f365b71340294b8217df21
makowens <59294988+makowens@users.noreply.github.com>
2020-01-10 14:54:43 -0500

    Third Commit

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -2,7 +2,7 @@
 {
    public class StandardGradeBook : BaseGradeBook
     {
-     public StandardGradeBook(string name)
+     public StandardGradeBook(string name) :base(name)
         {
             Type = GradeBookType.Standard;
         }
